7b38b10cfba4dd13cacdc3c9444aa5b2b1bb8cf8
Trevor Jordan <tsj7ww@gmail.com>
2020-09-17 12:56:30 -0400

    create Ping class for emailing

--- py/emailer.py
+++ py/emailer.py
@@ -5,6 +5,59 @@ from email.mime.application import MIMEApplication
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
+class Ping:
+    def __init__(self,subject,sender,recipients,server,body=None,files=None):
+        self.subject = subject
+        self.sender = sender
+        self.recipients = recipients
+        self.server = server
+        self.body = body
+        self.files = files
+        self.msg = _make(self)
+
+    def _make(self):
+        p = {
+            'Subject': self.subject,
+            'From': self.sender,
+            'To': self.recipients
+        }
+        if self.files:
+            try:
+                m = MIMEMultipart()
+            except:
+                raise
+            try:
+                for i,_ in enumerate(files):
+                    a = MIMEApplication(_['content'],Name=_['fname'])
+                    a['Content-Disposition'] = 'attachment; filename={}'.format(_['fname'])
+                    m.attach(a)
+            except Exception as e:
+                raise Exception('Error on processing file {}: {}'.format((i+1), str(e)))
+        else:
+            try:
+                m = EmailMessage()
+            except:
+                raise
+        if self.body:
+            try:
+                m.attach(MIMEText(self.body['content']), subtype=self.body['type'])
+            except Exception as e:
+                raise Exception('Body unable to be attached: {}'.format(str(e)))
+        for i,j in p.items():
+            try:
+                m[i] = j
+            except Exception as e:
+                raise Exception('Unable to set parameter {} on message: {}'.format(i,str(e)))
+        return m
+
+    def send(self):
+        try:
+            with smtplib.SMTP(self.server) as s:
+                s.send_message(self.msg)
+        except Exception as e:
+            raise Exception('Unable to send message: {}'.format(str(e)))
+
+
 def BODY(data):
     """"""
     _hr = datetime.datetime.now().hour
